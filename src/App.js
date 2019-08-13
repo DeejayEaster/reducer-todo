a4c1c0b70acd59c495f003d0e2aa9b89bcc7fc6e
deejayeaster <deejayeaster@live.com>
2019-08-13 17:03:06 -0400

    MVP + basic styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState, useReducer } from "react";
 import "./App.css";
+import { Card } from "semantic-ui-react";
+
+import { initialState, todoReducer } from "./reducer/todoReducer";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+
+  const handleChange = e => {
+    setTodo(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: "add-todos", payload: todo });
+    setTodo("");
+  };
+
   return (
     <div className="App">
-      <h1>Hey</h1>
+      <h1>Reduce Your Todos</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="todo"
+          value={todo}
+          onChange={handleChange}
+          placeholder="Enter a todo..."
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+      <button onClick={() => dispatch({ type: "clear-todos" })}>
+        Clear Completed
+      </button>
+      <div className="card-container">
+        {state.todos.map(todo => (
+          <Card
+            key={todo.id}
+            onClick={() => dispatch({ type: "toggle-todos", payload: todo })}
+          >
+            <p>{todo.item}</p>
+            <p>Done yet? {todo.completed ? "Yep" : "Nope"}</p>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
